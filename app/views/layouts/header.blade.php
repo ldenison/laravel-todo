bceb4ac716bdd8d60ab808e4049acaf16b82dcc0
Luke Denison <luke@Lukes-Mac-mini.local>
2014-12-08 11:49:59 -0500

    Vanilla Laravel installation with ember.js added

---
+++ app/views/layouts/header.blade.php
@@ -0,0 +1,11 @@
+@section("header")
+    This is the header for the application
+@show
+
+<html>
+    <head>
+        <title>Simple Todo list in Laravel and Ember.js</title>
+        {{ HTML::script("js/ember-1.8.1.min.js"); }}
+    </head>
+
+</html>
